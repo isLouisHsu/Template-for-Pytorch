3aae21d2b136ae6596bff2aebc50d4bf8b852d1f
louishsu <is.louishsu@foxmail.com>
2019-05-29 09:02:57 +0800

    src

--- src/datasets.py
+++ src/datasets.py
@@ -20,4 +20,32 @@ class Data(Dataset):
     def __len__(self):
 
         return 
-     
+
+class Prefetcher():
+
+    def __init__(self, dataloader):
+
+        self.dataloader = iter(dataloader)
+        self.stream = torch.cuda.Stream()
+        self._X, self._y = None, None
+        self._preload()
+    
+    def _preload(self):
+
+        try:
+            self._X, self._y = next(self.dataloader)
+        except StopIteration:
+            self._X, self._y = None, None
+            return
+        
+        with torch.cuda.stream(self.stream):
+            self._X = self._X.cuda(non_blocking=True)
+            self._y = self._y.cuda(non_blocking=True)
+    
+    def next(self):
+        
+        torch.cuda.current_stream().wait_stream(self.stream)
+        X = self._X; y = self._y
+        self._preload()
+        
+        return X, y
